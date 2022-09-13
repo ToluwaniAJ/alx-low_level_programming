a323d888760eea9e67adddd24483e3e9e11cd7f2
Toluwani Ajibare <toluwaniajibare@gmail.com>
2022-09-13 16:46:12 -0700

    Prints x10 the alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-
 	char i;
 	int j;
 
 	for (j = 1; j <= 10; j++)
 	{
 		 for (i = 'a'; i <= 'z'; i++)
-			  _putchar(i);
+			 _putchar(i);
 		  _putchar('\n');
 	}
 
