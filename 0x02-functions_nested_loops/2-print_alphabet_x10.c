7fb8a7771a6db365a9afc56f57cefae2114737f7
Toluwani Ajibare <toluwaniajibare@gmail.com>
2022-09-13 15:29:17 -0700

    A function that prints 10 times the alphabet followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10(void) - Entry point
+ * void print_alphabet_x10 - Entry point
  *
  * Description: _putchar function prints 10 times the alphabet followed by a new line
  *
