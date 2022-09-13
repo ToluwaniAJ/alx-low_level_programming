a57a82c00392ebb9d62e47faec3330c31d082dcc
Toluwani Ajibare <toluwaniajibare@gmail.com>
2022-09-13 14:34:21 -0700

    Prints alphabet in lowercase followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+
+	return (0);
+}
