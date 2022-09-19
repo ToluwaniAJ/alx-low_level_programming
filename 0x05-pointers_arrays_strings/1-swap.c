89da71b47c402e72fc32588d625fae3c8c2d2a83
Toluwani Ajibare <toluwaniajibare@gmail.com>
2022-09-19 13:32:06 -0700

    Added 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: an input integer
+ * @b: an input integer
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int 98;
+
+	98 = *a;
+	*a = *b;
+	*b = 98;
+}
+
