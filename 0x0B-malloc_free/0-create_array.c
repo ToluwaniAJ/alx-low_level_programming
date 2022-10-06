777b5de1ecee50771b12abc9eb9f5001b7562075
Toluwani Ajibare <toluwaniajibare@gmail.com>
2022-10-05 17:03:37 -0700

    Added 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * @c: a character
  * Return: A pointer to the array or NULL if it fails
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i = 0;
