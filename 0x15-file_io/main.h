5f4ddd9a33de1a09c4ffdce7501eb93e1de80841
Toluwani Ajibare <toluwaniajibare@gmail.com>
2022-11-01 02:19:59 +0100

    Added new file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
-#endif /* __MAIN_H__ */
+#endif 
